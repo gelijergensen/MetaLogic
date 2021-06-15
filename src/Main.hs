d6962fa8fb62328b3e71b0e1f6a71ffb57a113cb
G. Eli Jergensen <elijergensen@gmail.com>
2021-06-15 08:23:18 +0200

    Removed old PropLogicEval file

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,20 @@
 module Main where
 
 import Data.Either (fromRight)
+import qualified Data.Set as Set
 import Interpreter (interpret)
+import qualified LogicSystem as LS
 import Parser (parseAST)
 import PropositionalLogic
-import qualified PropositionalLogicEval as Prop
 
 main :: IO ()
 main = putStrLn "Main does run"
-
-x = Prop.And y (Prop.Not (Prop.Variable "C"))
-
-y = Prop.Or (Prop.Variable "A") Prop.Truth
 
 parseAsPropFormula =
   fromRight undefined
     . interpret defaultPropositionalLogicInterpreter
     . fromRight undefined
     . parseAST
+
+rewrite :: Ord a => PropFormula a -> Set.Set (PropFormula a)
+rewrite = LS.rewrite PropositionalLogic
